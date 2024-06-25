d5f84df01205962ca9dae2a3e74fea67ae8975d2
GustavoSD <gustavo.sdias12@gmail.com>
2024-06-25 15:09:00 -0300

    comit

--- JS/HeaderResponsivo.js
+++ JS/HeaderResponsivo.js
@@ -1,5 +1,18 @@
+
+
 function toggleMenu() 
 {
+
     let overlay = document.getElementById('overlay');
     overlay.style.display = "block";
+    let closeButton = document.getElementById('hamburguer');
+    closeButton.style.display = "block";
+}
+
+function fechar()
+{
+    let overlay = document.getElementById('overlay');
+    overlay.style.display = "none";
+    let closeButton = document.getElementById('hamburguer');
+    closeButton.style.display = "none";
 }
