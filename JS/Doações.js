ef7d2d6db46f5a2dd3618a9cefd795fed71d2041
Manoela Bello <166542868+manoela-bello@users.noreply.github.com>
2024-06-25 14:01:42 -0300

    página de doações

--- JS/Doações.js
+++ JS/Doações.js
@@ -1,43 +1,12 @@
-const Container = document.querySelector('#A');
-let Clicked = true;
-
-let showEhide = function(){
-    if(Clicked){
-        Container.style.display ='block';
-        Clicked = false;
-    }
-    else{
-        Container.style.display = 'none';
-        Clicked = true;
-    }
+function showDiv(className) {
+    // Esconde todas as divs de conteúdo
+    var contents = document.querySelectorAll('#contentArea .content');
+    contents.forEach(function(content) {
+        content.style.display = 'none';
+    });
+    
+    // Mostra a div de conteúdo correspondente ao botão clicado
+    var activeContent = document.querySelector('#contentArea .' + className);
+    activeContent.style.display = 'block';
 }
-
-const divContainer = document.querySelector('#B');
-let isClicked = true;
-
-let showOrHide = function(){
-    if(isClicked){
-        divContainer.style.display ='block';
-        isClicked = false;
-    }
-    else{
-        divContainer.style.display = 'none';
-        isClicked = true;
-    }
-}
-
-const Containery = document.querySelector('#C');
-let Clickedy = true;
-
-let showYhide = function(){
-    if(Clickedy){
-        Containery.style.display ='block';
-        Clickedy = false;
-    }
-    else{
-        Containery.style.display = 'none';
-        Clickedy = true;
-    }
-}
-
-
+s
